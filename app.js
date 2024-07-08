2245d424d5a5f68abb1c95929ba628f6aefb630a
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-08 22:01:41 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,69 @@
+// 電卓ウィンドウを開く関数
+function openCalculator() {
+  createWindow('電卓', calculatorContent());
+}
+
+// 電卓のコンテンツを設定する関数
+function calculatorContent() {
+  return `
+    <input type="text" id="calc-display" disabled>
+    <br>
+    <button onclick="inputCalc('1')">1</button>
+    <button onclick="inputCalc('2')">2</button>
+    <button onclick="inputCalc('3')">3</button>
+    <button onclick="inputCalc('+')">+</button>
+    <br>
+    <button onclick="inputCalc('4')">4</button>
+    <button onclick="inputCalc('5')">5</button>
+    <button onclick="inputCalc('6')">6</button>
+    <button onclick="inputCalc('-')">-</button>
+    <br>
+    <button onclick="inputCalc('7')">7</button>
+    <button onclick="inputCalc('8')">8</button>
+    <button onclick="inputCalc('9')">9</button>
+    <button onclick="inputCalc('*')">*</button>
+    <br>
+    <button onclick="inputCalc('0')">0</button>
+    <button onclick="inputCalc('/')">/</button>
+    <button onclick="inputCalc('=')">=</button>
+    <button onclick="inputCalc('C')">C</button>
+  `;
+}
+
+function inputCalc(value) {
+  const display = document.getElementById('calc-display');
+  if (value === 'C') {
+    display.value = '';
+  } else if (value === '=') {
+    display.value = eval(display.value);
+  } else {
+    display.value += value;
+  }
+}
+
+// タスクマネージャーウィンドウを開く関数
+function openTaskManager() {
+  createWindow('タスクマネージャー', taskManagerContent());
+}
+
+function taskManagerContent() {
+  return '<div id="task-list">タスク一覧をここに表示</div>';
+}
+
+// ブラウザウィンドウを開く関数
+function openBrowser() {
+  createWindow('windbrowser', browserContent());
+}
+
+function browserContent() {
+  return `
+    <input type="text" id="browser-url" placeholder="URLを入力">
+    <button onclick="loadUrl()">移動</button>
+    <iframe id="browser-frame" style="width: 100%; height: calc(100% - 30px);"></iframe>
+  `;
+}
+
+function loadUrl() {
+  const url = document.getElementById('browser-url').value;
+  document.getElementById('browser-frame').src = url;
+}
